a14dc8c8ca666a3145b4df1bfb9b17b2ff125cfa
Kevin-Lad <kevinladisheff@gmail.com>
2023-08-21 15:49:45 -0700

    notebook update

--- app.py
+++ app.py
@@ -24,8 +24,7 @@ Base.prepare(autoload_with=engine)
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
-# Create our session (link) from Python to the DB
-session = Session(engine)
+
 
 #################################################
 # Flask Setup
@@ -44,33 +43,37 @@ def welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/start/end<br/>" 
     )
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
+    session = Session(engine)
    
     # Query precipitation analysis
-    year_ago =  dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    recent_date_str = session.query(Measurement.date).order_by(Measurement.date.desc()).first()[0]
+    recent_date = dt.date.fromisoformat(recent_date_str)
 
-    results = session.query(Measurement.date, Measurement.prcp). \
-        filter(Measurement.date >= year_ago).all()
+    year_ago =  recent_date - dt.timedelta(days=365)
+
+    results = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= year_ago).\
+        all()
     
     session.close()
     
     # Create a dictionary from the row data and append to a list of precripitation
-    precipitation_list = []
-    for date, prcp in results:
-        precipitation_dict = {}
-        precipitation_dict["date"] = date
-        precipitation_dict["prcp"] = prcp
-        precipitation_list.append(precipitation_dict)
+    prcp_dict ={}
+    for result in results:
+        date = result[0]
+        prcp = result[1]
+        prcp_dict[date] = prcp
 
-    return jsonify(precipitation)
+    return jsonify(prcp_dict)
 
 @app.route("/api/v1.0/stations")
 def stations():
-
+    session = Session(engine)
     #Query the list of stations
     stations1 = session.query(Station.station).all()
     
@@ -82,25 +85,47 @@ def stations():
 
 @app.route("/api/v1.0/tobs")
 def tobs():
-
+    session = Session(engine)
     #Query the dates and temperature observations of the most active staions for the previous year
     most_active = 'USC00519281'
-    year_ago =  dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    recent_date_str = session.query(Measurement.date).order_by(Measurement.date.desc()).first()[0]
+    recent_date = dt.date.fromisoformat(recent_date_str)
 
-    date_temp_results = session.query(Measurement.date, Measurement.tobs) \
-                            .filter(Measurement.date >= year_ago) \
-                            .filter(Measurement.station == most_active).all()
+    year_ago =  recent_date - dt.timedelta(days=365)
+   
+
+    date_temp_results = session.query(Measurement.date, Measurement.tobs).\
+                            filter(Measurement.date >= year_ago).\
+                            filter(Measurement.station == most_active).\
+                            all()
 
     date_temp_list = list(np.ravel(date_temp_results))
 
     return jsonify(date_temp_list)
 
+@app.route("/api/v1.0/<start>/<end>")
+def get_date(start_date, end_date):
+    session = Session(engine)
+    # Query temperatures(min, max and avg) between certain dates
+    results = session.query(Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+    group_by(Measurement.date).\
+    filter(Measurement.date >= start_date).\
+    filter(Measurement.date <= end_date).\
+    all()
 
+    session.close()
 
+    all_temps = []
+    for date, min, avg, max in results:
+        temp_dict = {}
+        temp_dict['date'] = date,
+        temp_dict['min'] = min,
+        temp_dict['avg'] = avg,
+        temp_dict['max'] = max
+        all_temps.append(temp_dict)
+    
 
-
-
-
+    return jsonify(all_temps)
 
 
 
